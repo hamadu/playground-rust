279be333302ad2f3bd11c357a6925cd5724d1e98
hama_du <dercll1020@gmail.com>
2016-11-09 08:35:07 +0900

    implemented

--- README.md
+++ README.md
@@ -8,4 +8,7 @@ https://doc.rust-lang.org/1.7.0/book/guessing-game.html
 
 [primes]
 入力された整数以下の素数を表示する。行バッファI/Oの練習
+
+[segment_tree]
+モノイドを扱えるセグメントツリー。
 ```
--- segment_tree/segment_tree_monoid.rs
+++ segment_tree/segment_tree_monoid.rs
@@ -1,14 +1,47 @@
-use std::cmp::min;
-
 trait Monoid {
     fn unit() -> Self;
 
     fn merge(a: Self, b: Self) -> Self;
 }
 
+#[derive(Debug, Copy, Clone, PartialEq)]
 struct Rational {
-    p: i32,
-    q: i32
+    p: i64,
+    q: i64
+}
+
+impl Rational {
+    fn new(p: i64, q: i64) -> Rational {
+        Rational { p: p, q: q }
+    }
+}
+
+impl Monoid for Rational {
+    fn unit() -> Rational {
+        Rational { p: 1, q: 0 }
+    }
+
+    fn merge(a: Rational, b: Rational) -> Rational {
+        if a.p * b.q < a.q * b.p {
+            Rational { p: a.p, q: a.q }
+        } else {
+            Rational { p: b.p, q: b.q}
+        }
+    }
+}
+
+impl Monoid for i32 {
+    fn unit() -> i32 {
+        1000000000
+    }
+
+    fn merge(a: i32, b: i32) -> i32 {
+        if a < b {
+            a
+        } else {
+            b
+        }
+    }
 }
 
 struct SegmentTree<T> {
@@ -16,7 +49,7 @@ struct SegmentTree<T> {
     data: Vec<T>
 }
 
-impl<T: Monoid> SegmentTree<T>  {
+impl<T: Monoid + Copy> SegmentTree<T>  {
     fn new(v: Vec<T>) -> SegmentTree<T> {
         let size = v.len();
         let mut m = 1;
@@ -37,11 +70,11 @@ impl<T: Monoid> SegmentTree<T>  {
         SegmentTree { m: m, data: data }
     }
 
-    fn min(& self, range: (usize, usize)) -> T {
-        self.min_range(range, 0, (0, self.m))
+    fn find(& self, range: (usize, usize)) -> T {
+        self.find_range(range, 0, (0, self.m))
     }
 
-    fn min_range(& self, (query_l, query_r): (usize, usize), idx: usize, (seg_l, seg_r): (usize, usize)) -> T {
+    fn find_range(& self, (query_l, query_r): (usize, usize), idx: usize, (seg_l, seg_r): (usize, usize)) -> T {
         if query_r <= seg_l || seg_r <= query_l {
             return T::unit();
         }
@@ -50,8 +83,8 @@ impl<T: Monoid> SegmentTree<T>  {
         }
 
         let seg_med = (seg_l + seg_r) / 2;
-        let l = self.min_range((query_l, query_r), idx*2+1, (seg_l, seg_med));
-        let r = self.min_range((query_l, query_r), idx*2+2, (seg_med, seg_r));
+        let l = self.find_range((query_l, query_r), idx*2+1, (seg_l, seg_med));
+        let r = self.find_range((query_l, query_r), idx*2+2, (seg_med, seg_r));
         return T::merge(l, r)
     }
 
@@ -60,20 +93,42 @@ impl<T: Monoid> SegmentTree<T>  {
         self.data[pos] = value;
         while pos > 0 {
             pos = (pos-1)/2;
-            self.data[pos] = self.data[pos*2+1].merge(self.data[pos*2+2]);
+            self.data[pos] = T::merge(self.data[pos*2+1], self.data[pos*2+2]);
         }
     }
 }
 
 fn main() {
-    let mut seg = SegmentTree::new(vec![7,8,1,2,4,6,3,4], 100);
+    // use i32
+    let mut seg = SegmentTree::new(vec![
+        7,8,1,2,4,6,3,4
+    ]);
 
-    assert_eq!(seg.min((0, 4)), 1);
-    assert_eq!(seg.min((3, 6)), 2);
-    assert_eq!(seg.min((1, 2)), 8);
+    assert_eq!(seg.find((0, 4)), 1);
+    assert_eq!(seg.find((3, 6)), 2);
+    assert_eq!(seg.find((1, 2)), 8);
 
     seg.set(2, 5);
-    assert_eq!(seg.min((0, 8)), 2);
-    assert_eq!(seg.min((0, 3)), 5);
+    assert_eq!(seg.find((0, 8)), 2);
+    assert_eq!(seg.find((0, 3)), 5);
 
+
+    // use Rational
+    let mut seg = SegmentTree::new(vec![
+        Rational::new(1, 2),
+        Rational::new(2, 3),
+        Rational::new(1, 4),
+        Rational::new(1, 5),
+        Rational::new(1, 6),
+        Rational::new(1, 7),
+        Rational::new(1, 8),
+        Rational::new(1, 9)
+    ]);
+
+    assert_eq!(seg.find((0, 4)), Rational::new(1, 5));
+    assert_eq!(seg.find((3, 6)), Rational::new(1, 7));
+    assert_eq!(seg.find((1, 2)), Rational::new(2, 3));
+
+    seg.set(7, Rational::new(1, 5));
+    assert_eq!(seg.find((0, 8)), Rational::new(1, 8));
 }
